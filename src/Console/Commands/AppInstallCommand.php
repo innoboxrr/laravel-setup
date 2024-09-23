c7f21e2e855e645171d2c98ce6fa4682f295a49f
Homero Raul <h.raulvc@gmail.com>
2024-09-22 20:28:16 -0600

    Update

--- src/Console/Commands/AppInstallCommand.php
+++ src/Console/Commands/AppInstallCommand.php
@@ -77,20 +77,17 @@ class AppInstallCommand extends Command
             throw new \RuntimeException($process->getErrorOutput());
         }
     }
-    
 
     private function replaceProvidersFiles()
     {
         $appServiceProvider = file_get_contents(__DIR__ . '/../../../stubs/laravel/app/Providers/AppServiceProvider.php.stub');
         $authServiceProvider = file_get_contents(__DIR__ . '/../../../stubs/laravel/app/Providers/AuthServiceProvider.php.stub');
         $eventServiceProvider = file_get_contents(__DIR__ . '/../../../stubs/laravel/app/Providers/EventServiceProvider.php.stub');
-        $broadcastServiceProvider = file_get_contents(__DIR__ . '/../../../stubs/laravel/app/Providers/BroadcastServiceProvider.php.stub');
         $routeServiceProvider = file_get_contents(__DIR__ . '/../../../stubs/laravel/app/Providers/RouteServiceProvider.php.stub');
 
         file_put_contents(base_path('app/Providers/AppServiceProvider.php'), $appServiceProvider);
         file_put_contents(base_path('app/Providers/AuthServiceProvider.php'), $authServiceProvider);
         file_put_contents(base_path('app/Providers/EventServiceProvider.php'), $eventServiceProvider);
-        file_put_contents(base_path('app/Providers/BroadcastServiceProvider.php'), $broadcastServiceProvider);
         file_put_contents(base_path('app/Providers/RouteServiceProvider.php'), $routeServiceProvider);
     }
 
