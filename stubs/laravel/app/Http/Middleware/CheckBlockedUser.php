366639faf226482d3095c29319ae582458f2ae24
Homero Raul <h.raulvc@gmail.com>
2025-07-13 00:05:04 -0600

    Add middleware stubs for Admin, Authenticate, CheckBlockedUser, EncryptCookies, PreventRequestsDuringMaintenance, RedirectIfAuthenticated, SetLocale, TrimStrings, TrustHosts, TrustProxies, ValidateSignature, and VerifyCsrfToken

---
+++ stubs/laravel/app/Http/Middleware/CheckBlockedUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckBlockedUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (
+            method_exists(Auth::user(), 'isBlocked') && 
+            Auth::check() && 
+            Auth::user()->isBlocked()
+        ) {
+            abort(403, 'Your account has been blocked.');
+        }
+
+        return $next($request);
+    }
+}
